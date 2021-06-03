6bb749b58c57ad901f61d7977652a1aa8866c92a
20017533 <20017533@myrp.edu.sg>
2021-06-03 16:13:38 +0800

    latest

--- app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayActivity.java
+++ app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 public class ArrayActivity extends AppCompatActivity {
 
     TextView tv;
+    String[] fruits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +16,24 @@ public class ArrayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_array);
 
         tv = findViewById(R.id.textView1);
-
+        String []array=new String[3];
         //continue view the code in the worksheet
+        array[0] = "apple";
+        array[1] = "banana";
+        array[2] = "cherry";
 
+
+        System.out.println("Size is : " + array.length);
+
+        String msg = "";
+        for (int i = 0; i < array.length; i ++){
+            msg += array[i] + "\n";
+        }
+
+        System.out.println(msg);
+
+
+        tv.setText(msg);
 
 
     }
--- app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayListActivity.java
+++ app/src/main/java/sg/edu/rp/c346/day2/demoarraysarraylist/ArrayListActivity.java
@@ -5,19 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class ArrayListActivity extends AppCompatActivity {
 
     TextView tv;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_array_list);
 
         tv = findViewById(R.id.textView2);
-
+        ArrayList<String> al = new ArrayList<String>();
         //continue view the code in the worksheet
+        al.add("apple");
+        al.add("banana");
+        al.add("cherry");
+        al.add("durian");
 
+        al.set(-1,"dragon fruit");
+
+
+        System.out.println("Size is : " + al.size());
+
+        String msg = "";
+        for (int i = 0; i < al.size(); i ++){
+            msg += al.get(i) + "\n";
+        }
+
+        System.out.println(msg);
+        tv.setText(msg);
 
     }
 }
+
+
+
+
+
+
